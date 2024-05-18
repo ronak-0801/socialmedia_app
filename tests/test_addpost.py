88c135afe56d31f627ab25a2919a1a91603fc51a
Ronak Babariya <ronakbabariya555@gmail.com>
2024-05-18 12:50:43 +0530

    solved db session error

--- tests/test_addpost.py
+++ tests/test_addpost.py
@@ -36,16 +36,15 @@ def test_add_post_user_not_found(seed,auth_headers):
         'caption' : 'post 1 user 1',
         'total_like' : 0
     }
-    responce = client.post('/addpost', headers=auth_headers, json=data)
-    
-    print(responce.json())
-
-    assert "User is deleted or not found" in responce.json()
+    response = client.post('/addpost', headers=auth_headers, json=data)
+    print(response.json())
+    assert "User is deleted or not found" in response.json()
 
 
-# @pytest.mark.seed_data(('users_factory', {}), ('posts_factory', {}))
-# def test_get_posts():
-#     response = client.get("/allpost/")
+# # @pytest.mark.seed_data(('users_factory', {}), ('posts_factory', {}))
+# def test_get_posts(seed,auth_headers):
+#     response = client.get("/allpost/",headers=auth_headers)
+#     breakpoint()
 #     assert response.status_code == 200
 #     print(response.json())
 #     assert len(response.json()) > 0
